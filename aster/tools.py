de0d70f2fb0cae98b938fb38883128b6c569e77d
Vic Gupta <vicgupta@gmail.com>
2024-08-10 10:43:50 -0400

    Update tools.py
    added cosine_similarity

--- aster/tools.py
+++ aster/tools.py
@@ -63,3 +63,15 @@ class WebTools:
         except Exception as e:
             print("An error occurred:", e)
             return None
+            
+    # pip install -U scikit-learn
+    @staticmethod
+    def calculate_cosine_similarity(text1, text2):
+        from sklearn.feature_extraction.text import TfidfVectorizer
+        from sklearn.metrics.pairwise import cosine_similarity
+        texts = [text1, text2]
+        vectorizer = TfidfVectorizer()
+        tfidf_matrix = vectorizer.fit_transform(texts)
+        cosine_sim = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])    
+        return cosine_sim[0][0]
+
