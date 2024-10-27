c6993e449f295b75730de170f6661f1d6d370a08
Vic Gupta <vicgupta@gmail.com>
2024-10-27 09:10:30 -0400

    Update agents.py
    Added ReasoningAgent

--- aster/agents.py
+++ aster/agents.py
@@ -79,3 +79,44 @@ class Agent:
         # self._chat_history.append(message_prompt)
         self.message_prompt.append(create_prompt("assistant", response))
         return response
+
+class ReasoningAgent:
+    def __init__(
+        self,
+        llm: object,
+        custom_system_prompt: str = "You are an intelligent AI Assistant focused on clear reasoning and step-by-step analysis.  You break down complex problems methodically and explain your thought process transparently.  You aim to be precise, logical and thorough in your responses while maintaining a helpful and professional tone.",
+        format: str = "",
+        temperature: float = 0.5,
+        max_tokens: int = 1024,
+    ):
+
+        self._llm = llm
+        self._temperature = temperature
+        self._format = format.lower()
+        self._max_tokens = max_tokens
+        self.message_prompt = []
+        self.message_prompt.append(create_prompt("system", custom_system_prompt))
+
+    def ask(self, prompt):
+        self.message_prompt.append(create_prompt("user", prompt))
+
+        if isinstance(self._llm, OllamaModel):
+            response = self._llm.ask(
+                self.message_prompt,
+                temperature=self._temperature,
+                format=self._format,
+            )
+        if isinstance(self._llm, GroqModel):
+            response = self._llm.ask(
+                self.message_prompt,
+                temperature=self._temperature,
+                max_tokens=self._max_tokens,
+                format=self._format,
+            )
+        if isinstance(self._llm, OpenAIModel):
+            response = self._llm.ask(
+                self.message_prompt,
+            )
+
+        self.message_prompt.append(create_prompt("assistant", response))
+        return response
