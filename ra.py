864951f431276d7f82cd0eeae7f1a5a731cddb62
Vic Gupta <vicgupta@gmail.com>
2024-10-27 09:25:34 -0400

    deleted ra

--- ra.py
+++
@@ -1,6 +0,0 @@
-from aster.models import OllamaModel
-from aster.agents import Agent, ReasoningAgent
-
-llm = OllamaModel(model="llama3.1")
-ra = ReasoningAgent(llm, max_tokens=1024)
-print (ra.ask("What is the capital of France?"))
